0fb5b608c057715707a86ddd84aa670285c282c3
León Domingo <leon.domingo@ender.es>
2011-08-18 21:44:44 +0200

    Fixed "permissions" view

--- sapns/lib/sapns/update/init/sp_permission.view.sql
+++ sapns/lib/sapns/update/init/sp_permission.view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW _view_sp_permission AS 
-  SELECT perm.id, perm.name AS "Name", act.type AS "Type", 
+  SELECT perm.id, perm.name AS "Name", perm.type AS "Type", 
     perm.url AS "URL", cls.name AS "Class"
   FROM sp_permission perm
-  JOIN sp_classes cls ON cls.id = act.id_class;
+  JOIN sp_classes cls ON cls.id = perm.id_class;
