1f60f5a46153de85e0de464452edbd03ec780ade
León Domingo <leon.domingo@ender.es>
2011-12-07 19:57:09 +0100

    First version

--- sapns/controllers/root.py
+++ sapns/controllers/root.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 """Main Controller"""
 
-from tg import expose, flash, require, url, request, redirect, config
-from pylons.i18n import ugettext as _ #, lazy_ugettext as l_
-from tg.i18n import set_lang, get_lang
+from pylons.i18n import ugettext as _
 from repoze.what import predicates
-
+from sapns.controllers.dashboard import DashboardController
+from sapns.controllers.error import ErrorController
 from sapns.lib.base import BaseController
 from sapns.model import DBSession as dbs
 from sapns.model.sapnsmodel import SapnsUser
-
-from sapns.controllers.error import ErrorController
-from sapns.controllers.dashboard import DashboardController
-
+from sqlalchemy.sql.expression import or_, func
+from tg import expose, flash, require, url, request, redirect, config
+from tg.i18n import set_lang, get_lang
 import logging
 
 __all__ = ['RootController']
@@ -109,3 +107,62 @@ class RootController(BaseController):
         came_from = kw.get('came_from')
         if came_from:
             redirect(url(came_from))
+            
+    @expose('json')
+    def remember_password(self, username_or_email):
+        
+        from neptuno.enviaremail import enviar_email
+        import random
+        import hashlib as hl
+        
+        logger = logging.getLogger('RootController.remember_passsword')
+        try:
+            u = dbs.query(SapnsUser).\
+                filter(or_(func.upper(SapnsUser.user_name) == func.upper(username_or_email),
+                           func.upper(SapnsUser.email_address) == func.upper(username_or_email),
+                           )).\
+                first()
+                
+            if not u:
+                return dict(status=False, 
+                            message=_('No user has been found'))
+                
+            # generate a random password
+            random.seed()
+            s1 = hl.sha1('%6.6d' % random.randint(0, 999999))
+            
+            new_password = ''
+            for c in s1.hexdigest()[:random.randint(10, 15)]:
+                if random.randint(0, 1):
+                    new_password += c.upper()
+                    
+                else:
+                    new_password += c
+            
+            u.password = new_password
+            dbs.add(u)
+            dbs.flush()
+            
+            dst = [(u.email_address.encode('utf-8'), u.user_name.encode('utf-8'),)]
+                
+            # get e-mail settings
+            remitente = (config.get('avisos.e_mail'), config.get('avisos.nombre'),)
+            
+            # TODO: get e-mail template
+            asunto = _('Remember password')
+            mensaje = _('Your new password is %s') % new_password 
+            mensaje_html = mensaje
+            
+            email_login = config.get('avisos.login')
+            email_password = config.get('avisos.password')
+            
+            # send e-mail
+            enviar_email(remitente, dst, asunto, mensaje, 
+                         config.get('avisos.smtp'), email_login, email_password, 
+                         html=mensaje_html, charset='utf-8')
+            
+            return dict(status=True)
+    
+        except Exception, e:
+            logger.error(e)
+            return dict(status=False)            
