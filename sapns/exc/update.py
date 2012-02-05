1914f314d88b113c17e6cc6b9725f32e1a3b23dc
León Domingo <leon.domingo@ender.es>
2012-02-05 13:23:46 +0100

    New "executor"

--- sapns/exc/update.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from executor import Executor
-    
-if __name__ == '__main__':
-    
-    e = Executor()
-    e.execute('sapns.exc.update.update', 'Update')
