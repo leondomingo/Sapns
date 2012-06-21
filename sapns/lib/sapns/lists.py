4159143a1c28617f9482e2307feaee1c363a5dab
León Domingo <leon.domingo@ender.es>
2012-06-21 14:58:10 +0200

    Override table lists

---
+++ sapns/lib/sapns/lists.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+
+from neptuno.util import get_paramw, strtodate
+from pylons.i18n import ugettext as _
+from sapns.config import app_cfg
+from sapns.lib.sapns.util import pagination
+from sapns.model import DBSession as dbs, SapnsUser, SapnsClass, SapnsPermission
+from tg import url, request, redirect, config
+import logging
+import simplejson as sj
+from neptuno.postgres.search import Search
+
+date_fmt = config.get('formats.date', default='%m/%d/%Y')
+_strtodate = lambda s: strtodate(s, fmt=date_fmt, no_exc=True)
+
+class List(object):
+    
+    def __init__(self, cls, **kw):
+        self.logger = logging.getLogger('sapns.lib.sapns.lists.List')
+        
+        self.cls = cls
+        self.kw = kw
+
+        self.q = get_paramw(kw, 'q', unicode, opcional=True, por_defecto='')
+        self.rp = get_paramw(kw, 'rp', int, opcional=True, por_defecto=10)
+        self.pag_n = get_paramw(kw, 'pag_n', int, opcional=True, por_defecto=1)
+
+        came_from = kw.get('came_from', '')
+        if came_from:
+            came_from = url(came_from)
+            
+        self.came_from = came_from
+            
+        # collections
+        self.ch_attr = kw.get('ch_attr')
+        self.parent_id = kw.get('parent_id')
+    
+    def __call__(self):
+        
+        # does this user have permission on this table?
+        user = dbs.query(SapnsUser).get(int(request.identity['user'].user_id))
+        permissions = request.identity['permissions']
+        roles = request.identity['groups']
+        
+        cls_ = SapnsClass.by_name(self.cls)
+        ch_cls_ = SapnsClass.by_name(self.cls, parent=False)        
+            
+        if not user.has_privilege(cls_.name) or \
+        not '%s#%s' % (cls_.name, SapnsPermission.TYPE_LIST) in permissions:
+            redirect(url('/message', 
+                         kw=dict(message=_('Sorry, you do not have privilege on this class'),
+                                     came_from=self.came_from)))
+            
+        # shift enabled
+        shift_enabled_ = u'managers' in roles
+
+        
+        # related classes
+        rel_classes = cls_.related_classes()
+        
+        # collection
+        caption = ch_cls_.title
+        if self.ch_attr and self.parent_id:
+            
+            p_cls = cls_.attr_by_name(self.ch_attr).related_class
+            p_title = SapnsClass.object_title(p_cls.name, self.parent_id)
+            
+            caption = _('%s of [%s]') % (ch_cls_.title, p_title)
+            
+        return dict(page=_('list of %s') % ch_cls_.title.lower(), came_from=self.came_from,
+                    grid=dict(cls=ch_cls_.name,
+                              caption=caption,
+                              q=self.q.replace('"', '\\\"'), rp=self.rp, pag_n=self.pag_n,
+                              # collection
+                              ch_attr=self.ch_attr, parent_id=self.parent_id,
+                              # related classes
+                              rel_classes=rel_classes,
+                              shift_enabled=shift_enabled_,
+                              ))
+        
+    def grid(self):
+        """
+        OUT
+          {
+           status: <bool>,
+           cols: [{title: <str>, width: <int>, align: <str>}, ...],
+           data: [[<unicode>, ...], [<unicode>, ...], ...],
+           styles: [<str>, ...],
+           this_page: <int>,
+           total_count: <int>,
+           total_pag: <int>
+          }
+        """
+        
+        ds = self.grid_data()        
+        
+        # Reading global settings
+        ds.date_fmt = date_fmt
+        ds.time_fmt = config.get('formats.time', default='%H:%M')
+        ds.datetime_fmt = config.get('formats.datetime', default='%m/%d/%Y %H:%M')
+        ds.true_const = _('Yes')
+        ds.false_const = _('No')
+        
+        ds.float_fmt = app_cfg.format_float
+        
+        visible_width = 800
+        min_width = visible_width / 6
+        
+        default_width = visible_width / len(ds.labels)
+        if default_width < min_width:
+            default_width = min_width
+        
+        cols = []
+        for col in ds.labels:
+            w = default_width
+            if col == 'id':
+                w = 60
+                
+            cols.append(dict(title=col, width=w, align='center'))
+            
+        this_page, total_pag = pagination(self.rp, self.pag_n, ds.count)
+        
+        if ds.count == self.rp:
+            total_pag = self.pag_n + 1
+        
+        return dict(status=True, cols=cols, data=ds.to_data(), styles=[],
+                    this_page=this_page, total_count=ds.count, total_pag=total_pag)
+        
+    def grid_data(self):
+        
+        _logger = logging.getLogger('DashboardController.grid_data')
+
+        pos = (self.pag_n-1) * self.rp
+        
+        # filters
+        filters = get_paramw(self.kw, 'filters', sj.loads, opcional=True)
+
+        cls_ = SapnsClass.by_name(self.cls)
+        ch_cls_ = SapnsClass.by_name(self.cls, parent=False)        
+            
+        # does this user have permission on this table?
+        user = dbs.query(SapnsUser).get(int(request.identity['user'].user_id))
+        permissions = request.identity['permissions']
+             
+        if not user.has_privilege(cls_.name) or \
+        not '%s#%s' % (cls_.name, SapnsPermission.TYPE_LIST) in permissions:
+            return dict(status=False, 
+                        message=_('Sorry, you do not have privilege on this class'))
+        
+        # get view name
+        view = user.get_view_name(ch_cls_.name)
+            
+        # collection
+        col = None
+        if self.ch_attr and self.parent_id:
+            col = (self.cls, self.ch_attr, self.parent_id,)
+
+        # get dataset
+        _search = Search(dbs, view, strtodatef=_strtodate)
+        _search.apply_qry(self.q.encode('utf-8'))
+        _search.apply_filters(filters)
+        
+        return _search(rp=self.rp, offset=pos, collection=col, no_count=True)        
