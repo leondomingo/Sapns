2581d774f138d60f1254c3d5432f1868aa5861bc
León Domingo <leon.domingo@ender.es>
2012-07-30 15:55:27 +0200

    Sapns MailSender: manage "from" properly / Remove attachments

--- sapns/lib/sapns/mailsender.py
+++ sapns/lib/sapns/mailsender.py
@@ -10,6 +10,12 @@ import re
 
 class MailSender(object):
     
+    APP_MAILSENDER__MAIL = 'app.mailsender.mail'
+    APP_MAILSENDER__NAME = 'app.mailsender.name'
+    APP_MAILSENDER__LOGIN = 'app.mailsender.login'
+    APP_MAILSENDER__PASSWORD = 'app.mailsender.password'
+    APP_MAILSENDER__SMTP = 'app.mailsender.smtp'
+    
     def __init__(self, **kw):
         self.logger = logging.getLogger('MailSender')
         self.params = kw
@@ -19,9 +25,25 @@ class MailSender(object):
         # scheduled task
         task = dbs.query(SapnsScheduledTask).get(self.params['stask_id'])
         
-        # from
-        from_ = (self.params['from']['address'].encode('utf-8'), 
-                 self.params['from']['name'].encode('utf-8'),)
+        # from / smtp connection
+        from_address = config.get(self.APP_MAILSENDER__MAIL)
+        from_name = config.get(self.APP_MAILSENDER__NAME)
+        if self.params.get('from'):
+            from_ = (self.params['from'].get(['address'], from_address).encode('utf-8'),
+                     self.params['from'].get(['name'], from_name).encode('utf-8'),)
+            
+            # smtp
+            server = self.params['from'].get('smtp', config.get(self.APP_MAILSENDER__SMTP))
+            login = self.params['from'].get('login', config.get(self.APP_MAILSENDER__LOGIN))
+            password = self.params['from'].get('password', config.get(self.APP_MAILSENDER__PASSWORD))
+            
+        else:
+            from_ = (from_address, from_name,)
+            
+            # smtp
+            server = config.get(self.APP_MAILSENDER__SMTP)
+            login = config.get(self.APP_MAILSENDER__LOGIN)
+            password = config.get(self.APP_MAILSENDER__PASSWORD)
         
         def _dst_list(list_):
             r = []
@@ -43,24 +65,27 @@ class MailSender(object):
         if self.params.get('bcc'):
             bcc = _dst_list(self.params['bcc'])
             
-        # smtp connection
-        server = self.params['from'].get('smtp', config.get('app.mailsender.smtp'))
-        login = self.params['from'].get('login', config.get('app.mailsender.login'))
-        password = self.params['from'].get('password', config.get('app.mailsender.password'))
-        
         # collect attachments
         try:
             files = []
+            files_remove = []
             for doc in SapnsDoc.get_docs('sp_scheduled_tasks', task.scheduledtask_id):
                 f = open(os.path.join(doc.repo.abs_path(), doc.filename).encode('utf-8'), 'rb')
                 fn = (u'%s.%s' % (doc.title, doc.docformat.extension)).encode('utf-8')
                 fn = re.sub(r'[^a-z0-9_\-\.]', '_', fn.lower())
                 files.append((f, fn,))
                 
+                if self.params.get('remove_attachments'):
+                    files_remove.append(doc.doc_id)
+                    
             send_mail(from_, to_, self.params['subject'], self.params['message']['text'],
                       server, login, password, files=files, 
                       html=self.params['message'].get('html'),
                       cc=cc, bcc=bcc)
+            
+            # remove attachments
+            for id_doc in files_remove:
+                SapnsDoc.delete_doc(id_doc)            
             
         finally:
             for f, _ in files:
