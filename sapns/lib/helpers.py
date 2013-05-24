209986823949ff28721b727b119cac756488f532
León Domingo <leon.domingo@ender.es>
2013-05-24 13:41:36 +0200

    Added new features to "helpers"

--- sapns/lib/helpers.py
+++ sapns/lib/helpers.py
@@ -3,11 +3,23 @@
 """WebHelpers used in sapns."""
 
 from webhelpers import date, feedgenerator, html, number, misc, text
-from tg import predicates
-from sapns.util import ROLE_MANAGERS
+from tg import predicates, config
+from sapns.util import ROLE_MANAGERS, datetostr
+
+def is_manager():
+    return predicates.in_group(ROLE_MANAGERS)
+
+def allowed(permission_name):
+    return predicates.has_permission(permission_name)
+
+def root_folder():
+    return config.get('app.root_folder')
+
+def today():
+    import datetime as dt
+    return datetostr(dt.date.today())
 
 def url2(url):
-    from tg import config
     import urllib
     
     base_url = config.get('app.url')
