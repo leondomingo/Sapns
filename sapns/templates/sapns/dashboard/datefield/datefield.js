0ef3ff00456d619a1e9e4d71367b161a12026f31
León Domingo <leon.domingo@ender.es>
2013-06-27 18:30:17 +0200

    A better way to convert a text field into a date field based on .ini parameters (format) and language

---
+++ sapns/templates/sapns/dashboard/datefield/datefield.js
@@ -0,0 +1,41 @@
+var sapns_date_field = function(selector) {
+    $(selector).each(function() {        
+        // date format settings 
+        var first_day = "{{tg.config.get('js.first_day', '1')}}";
+        
+        // "datepicker" setting 
+        var date_fmt0 = "{{tg.config.get('js.date_format', 'mm/dd/yyyy')}}";
+        
+        // "format date library" setting 
+        var date_fmt1 = "{{tg.config.get('js.date_format2', 'mm/dd/yyyy')}}";
+        
+        if (!$(this).is('[disabled]')) {
+            $(this).datepicker({
+                firstDay: first_day,
+                changeMonth: true,
+                changeYear: true,
+                showOn: 'button',
+                autoSize: true,
+                nextText: "{{_('Next')}}",
+                prevText: "{{_('Prev')}}",
+                dateFormat: date_fmt0,
+                dayNames: ["{{_('Sunday')}}", "{{_('Monday')}}", "{{_('Tuesday')}}",
+                           "{{_('Wednesday')}}", "{{_('Thursday')}}", "{{_('Friday')}}",
+                           "{{_('Saturday')}}"],
+                dayNamesMin: ["{{_('Su')}}", "{{_('Mo')}}", "{{_('Tu')}}", "{{_('We')}}",
+                              "{{_('Th')}}", "{{_('Fr')}}", "{{_('Sa')}}"],
+                monthNames: ["{{_('January')}}", "{{_('February')}}", "{{_('March')}}",
+                             "{{_('April')}}", "{{_('May')}}", "{{_('June')}}",
+                             "{{_('July')}}", "{{_('August')}}", "{{_('September')}}", 
+                             "{{_('October')}}", "{{_('November')}}", "{{_('December')}}"],
+                monthNamesShort: ["{{_('Jan')}}", "{{_('Feb')}}", "{{_('Mar')}}",
+                                  "{{_('Apr')}}", "{{_('May_')}}", "{{_('Jun')}}",
+                                  "{{_('Jul')}}", "{{_('Aug')}}", "{{_('Sep')}}",
+                                  "{{_('Oct')}}", "{{_('Nov')}}", "{{_('Dec')}}"],
+           });
+        }
+        else {
+            $(this).css('width', '85px');
+        }
+    });
+}
